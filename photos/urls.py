1ab451e6616a8b051300b557a10c4492c9337eb1
fattym <jnganga288@gmail.com>
2020-05-25 15:54:02 +0300

    added urls changed files

--- photos/urls.py
+++ photos/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
-from .views import SearchResultsListView,SearchLocationListView,about
+from .views import SearchResultsListView,SearchLocationListView
 
 urlpatterns=[
     path('',views.home,name='home'),
